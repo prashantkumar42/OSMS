e7711cbaa2777d3691c0d838499a73e55f8bcf83
Pranshu Gupta <Pranshu Gupta>
2016-12-19 14:36:50 +0530

    handled the valuesof checkboxes in search filters

--- datacloud/datacloud/static/static_dirs/scripts/search.js
+++ datacloud/datacloud/static/static_dirs/scripts/search.js
@@ -5,14 +5,28 @@ function activateSearch() {
     document.getElementById("search").style = "background-color:darkgreen";
 }
 
+isbatch = 0;
+isgender = 0;
+isaddress = 0;
+isfee = 0;
+
+function toggle_isbatch() {
+    isbatch = (isbatch + 1)%2;
+}
+function toggle_isgender() {
+    isgender = (isgender + 1)%2;
+}
+function toggle_isaddress() {
+    isaddress = (isaddress + 1)%2;
+}
+function toggle_isfee() {
+    isfee = (isfee + 1)%2;
+}
+
 function search() {
-    isbatch = document.getElementById("checksbatch").value;
     batch = document.getElementById("sbatch").value;
-    isgender = document.getElementById("checksgender").value;
     gender = document.getElementById("sgender").value;
-    isaddress = document.getElementById("checksaddress").value;
     address = document.getElementById("saddress").value;
-    isfee = document.getElementById("checksfee").value;
     text = isbatch + " " + batch + " " + isgender + " " + gender + " " + isaddress + " " + address + " " + isfee;
     alert(text);
 }
