82883d42764ea5cc935fd6de6b569b7493df4dd3
Pranshu Gupta <pranshug258@outlook.com>
2017-03-22 02:32:33 +0530

    moved to mysql

--- datacloud/services/models.py
+++ datacloud/services/models.py
@@ -2,46 +2,46 @@ from django.db import models
 from django.utils import timezone
 
 class Student(models.Model):
-	name = models.CharField(max_length=255)
-	father = models.CharField(max_length=255)
-	mother = models.CharField(max_length=255)
-	batch = models.IntegerField()
-	contact = models.CharField(max_length=255)
-	age = models.IntegerField()
-	GENDERS = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'))
-	gender = models.CharField(max_length=1, choices=GENDERS)
-	address = models.CharField(max_length=2048)
+    name = models.CharField(max_length=255)
+    father = models.CharField(max_length=255)
+    mother = models.CharField(max_length=255)
+    batch = models.IntegerField()
+    contact = models.CharField(max_length=255)
+    age = models.IntegerField()
+    GENDERS = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'))
+    gender = models.CharField(max_length=6, choices=GENDERS)
+    address = models.CharField(max_length=2048)
 
-	def __str__(self):
-		text = self.name + ", " + self.father + ", " + self.mother + ", " + str(self.batch) + ", " + self.contact + ", " + str(self.age) + ", " + self.gender + ", " + self.address
-		return text
+    def __str__(self):
+        text = self.name + ", " + self.father + ", " + self.mother + ", " + str(self.batch) + ", " + self.contact + ", " + str(self.age) + ", " + self.gender + ", " + self.address
+        return text
 
 class Batch(models.Model):
-	name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Fee(models.Model):
-	studentId = models.IntegerField()
-	installments = models.IntegerField()
-	amountPerInst = models.IntegerField()
-	paidInst = models.IntegerField()
+    studentId = models.IntegerField()
+    installments = models.IntegerField()
+    amountPerInst = models.IntegerField()
+    paidInst = models.IntegerField()
 
-	def __str__(self):
-		return str(self.studentId)
+    def __str__(self):
+        return str(self.studentId)
 
 class Course(models.Model):
-	name = models.CharField(max_length=255)
-	batch = models.IntegerField()
+    name = models.CharField(max_length=255)
+    batch = models.IntegerField()
 
-	def __str__(self):
-		return (str(self.batch) + " " + self.name) 
+    def __str__(self):
+        return (str(self.batch) + " " + self.name) 
 
 class Grades(models.Model):
-	studentId = models.IntegerField()
-	courseID = models.IntegerField()
-	letterGrade = models.CharField(max_length=1)
+    studentId = models.IntegerField()
+    courseID = models.IntegerField()
+    letterGrade = models.CharField(max_length=1)
 
-	def __str__(self):
-		return (str(self.studentId) + ", " + str(self.courseID) + ": " + self.letterGrade) 
+    def __str__(self):
+        return (str(self.studentId) + ", " + str(self.courseID) + ": " + self.letterGrade) 
