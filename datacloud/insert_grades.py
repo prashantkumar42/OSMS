8994e35f3b6403dad3ab5f053f4588564b3a7ca2
Prashant Kumar <prshntkiitk@gmail.com>
2017-04-15 17:01:09 +0530

    finished insert_grade.py

---
+++ datacloud/insert_grades.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from services import models
+import numpy as np
+
+
+batches = models.Batch.objects.all()
+print("hello")
+print(len(batches))
+
+for b in batches:
+    gradeList = []
+    print(b.name)
+    students = models.Student.objects.filter(batch__id=b.pk)
+    courses = models.Course.objects.filter(batch__id=b.pk)
+    print(courses)
+    for s in students:
+        for c in courses:
+            tmpGrade = np.random.randint(0, 3)
+            g = models.Grades(student=s, course=c, grade=tmpGrade)
+            gradeList.append(g)
+
+    # insert grade per batch
+    models.Grades.object.bulk_create(gradeList)
